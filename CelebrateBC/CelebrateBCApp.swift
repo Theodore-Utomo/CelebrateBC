7d8aff17806459dd5d327c91bca315023bdb8356
TheodoreUtomo <theodoreutomo@gmail.com>
2024-09-09 16:37:54 -0400

    Initial Commit

---
+++ CelebrateBC/CelebrateBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  CelebrateBCApp.swift
+//  CelebrateBC
+//
+//  Created by Theodore Utomo on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CelebrateBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CelebrateBCTests/CelebrateBCTests.swift
@@ -0,0 +1,17 @@
+//
+//  CelebrateBCTests.swift
+//  CelebrateBCTests
+//
+//  Created by Theodore Utomo on 9/9/24.
+//
+
+import Testing
+@testable import CelebrateBC
+
+struct CelebrateBCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
