4db1ea3c7e4dbc11a22ddd9d72ebd363243b6c61
TheodoreUtomo <theodoreutomo@gmail.com>
2024-09-09 23:27:48 -0400

    Added Buttons

--- CelebrateBC/ContentView.swift
+++ CelebrateBC/ContentView.swift
@@ -8,12 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = ""
+    @State private var imageName = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 350, height: 350)
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.maroonBC)
+                .frame(height: 80)
+            Spacer()
+            HStack {
+                Button() {
+                    imageName = "eagleLogo"
+                    messageString = "We Are BC!"
+                } label: {
+                    Image(systemName: "graduationcap")
+                    Text("Who Are We?")
+                }
+                
+                Button("🦅 How is Baldwin?") {
+                    imageName = "baldwin"
+                    messageString = "Flyin' High!"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .foregroundStyle(.maroonBC)
+            .fontWeight(.bold)
+            .tint(.goldBC)
         }
         .padding()
     }
